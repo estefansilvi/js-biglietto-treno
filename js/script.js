fca23a3e5dcefa085993d68491ef50a93044f47a
Stefania <estefansilvi@gmail.com>
2020-12-14 18:54:33 +0100

    js biglietto finito e corretto

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@ var age = prompt('Quanti hanni ha?');
 var distance = prompt('Quanti km deve percorrere?');
 
 //CONNECT TO HTML TAG
-var priceTag = document.getElementById('priceTag');
+var price = document.getElementById('price');
+
+if (isNaN(distance) || isNaN(age) ) {
+alert('Inserire solo numeri');
+}else{
+
+var price =parseInt(distance) * 0.21;
+console.log(price + '£');
+}
 
 //CALCULATES DEFAULT PRICE
+var sconto;
 var pricePerKm = 0.21;
 
-var customerPrice = pricePerKm * distance;
+var price = (pricePerKm * distance);
 
-
-if (age < 18) {//UNDER 18 GETS 20% DISCOUNT
-  customerprice = ((pricePerKm / 100) * 20) * distance;
-} else if (age < 18) {//OVER65 GETS 40% DISCOUNT
-  customerPrice = ((pricePerKm / 100) * 40) * distance;
+if (age < 18) {
+ sconto = (price * 20) / 100;
+}else if (age > 65) {
+ sconto = (price * 40) / 100;
+}else{
+  sconto = 0;
 }
+
+var finalPrice = price - sconto;
+document.getElementById('price').innerHTML = finalPrice;
